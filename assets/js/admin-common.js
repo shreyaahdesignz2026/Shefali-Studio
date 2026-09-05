3b32ad9dbd9ab7ac719b4cf4b9db4170a4def507
AAArithrAAA108 <bis.arithra65@gmail.com>
2026-09-05 19:42:09 +0530

    Add admin login page and Supabase Auth session guard

---
+++ assets/js/admin-common.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  var SUPABASE_URL = 'https://lektufytmhaumsltyfxf.supabase.co';
+  var SUPABASE_ANON_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxla3R1Znl0bWhhdW1zbHR5ZnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MDU4ODcsImV4cCI6MjEwNDE4MTg4N30.NjFKO-hgk5PrdM-di5lOGbFYVXLWJtKpl24ye3GKAmk';
+
+  var client = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  window.SBTAdmin = {
+    client: client,
+    requireSession: function (callback) {
+      client.auth.getSession().then(function (res) {
+        var session = res.data && res.data.session;
+        if (!session) {
+          window.location.href = '/admin/';
+          return;
+        }
+        callback(session);
+      });
+    },
+    logout: function () {
+      client.auth.signOut().then(function () {
+        window.location.href = '/admin/';
+      });
+    },
+  };
+})();
